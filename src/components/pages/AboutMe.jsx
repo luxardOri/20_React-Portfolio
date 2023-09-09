082040839d2843e8121cde4f2c980a3ef2085a96
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-09-09 14:55:26 -0700

    updated about me page, added in image

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -1,13 +1,38 @@
 import React, { useState } from "react";
 import Header from "../Header";
 import Footer from "../Footer";
+import myImage from "../../assets/images/tsuki2.jpeg";
 
 function AboutMe() {
   const fontColor = "white";
   return (
     <div>
       <Header title="About Me" bgColor="#B07C9E" />
-      <div style={{ color: fontColor }}> This is where about me goes</div>
+      <br />
+      <img
+        src={myImage}
+        alt="Description of the image"
+        style={{ width: "200px", height: "150px" }}
+      />
+      <div style={{ color: fontColor }}>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex dignissimos
+        non error, facilis atque molestiae cum voluptate aut beatae sit a nisi
+        doloribus repudiandae accusamus earum porro incidunt. Eaque, voluptates!
+      </div>
+      <div style={{ color: fontColor }}>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex dignissimos
+        non error, facilis atque molestiae cum voluptate aut beatae sit a nisi
+        doloribus repudiandae accusamus earum porro incidunt. Eaque, voluptates!
+      </div>
+      <div style={{ color: fontColor }}>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex dignissimos
+        non error, facilis atque molestiae cum voluptate aut beatae sit a nisi
+        doloribus repudiandae accusamus earum porro incidunt. Eaque, voluptates!
+      </div>
+      <br />
       <Footer title="About Me" bgColor="#B07C9E" />
     </div>
   );
