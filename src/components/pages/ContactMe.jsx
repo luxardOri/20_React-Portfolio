2027007ad76ca0126a516b2e66964a915e28f88d
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-09-09 16:42:42 -0700

    added in form items on the contact me page

--- src/components/pages/ContactMe.jsx
+++ src/components/pages/ContactMe.jsx
@@ -3,11 +3,78 @@ import Header from "../Header";
 import Footer from "../Footer";
 
 function ContactMe() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "name") {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setName("");
+    setMessage("");
+    setEmail("");
+  };
   const fontColor = "white";
   return (
     <div>
       <Header title="Contact Me" bgColor="#B07C9E" />
-      <div style={{ color: fontColor }}> This is where about me goes</div>
+      <div style={{ color: fontColor }}>
+        <form className="form">
+          <input
+            value={name}
+            name="name"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Name"
+          />{" "}
+          <br />
+          <input
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="email"
+          />
+          <br />
+          <input
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            type="message"
+            placeholder="Message"
+          />
+          <br />
+          <button type="button" onClick={handleFormSubmit}>
+            Submit
+          </button>
+        </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+      </div>
       <Footer title="Contact Me" bgColor="#B07C9E" />
     </div>
   );
